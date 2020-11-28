b2428d6c92533909c346a2e024fea44b9e6b0cb5
yurii <jurakulek@gmail.com>
2020-11-28 20:07:51 +0200

    Updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 #!/bin/sh
 
+
+apt-get update && apt-get install -y netcat
+
+echo "Waiting for neo4j..."
+
+while ! nc -z "$DATABASE_HOST" "$DATABASE_PORT"; do
+  sleep 0.1
+done
+
+echo "Neoj4 started"
+
 poetry run gunicorn main:app -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8002 --workers=4 --log-level=info
 
 exec "$@"
